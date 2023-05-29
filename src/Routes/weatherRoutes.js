8b825e5943e5d353ca6bc0c7bd64ec57a2a98396
FevenSeyfu <fevenseyfu00@gmail.com>
2023-05-30 02:51:14 +0300

    Update Reducers

--- src/Routes/weatherRoutes.js
+++ src/Routes/weatherRoutes.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Detail from '../components/Detail';
 import Home from '../components/Home';
-import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 
 const weatherRoutes = () => (
   <>
-    <Header />
+    <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/weather" element={<Detail />} />
